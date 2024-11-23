bd31846b55c521d99ff276c5f555b65dae3d450f
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-11-23 18:59:23 +0200

    Add connection capabilities

---
+++ enums/connect.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Connect(Enum):
+    PLATFORM_NAME = 'Android'
+    DEVICE_NAME_MOBILE = '146ce6c8'
+    DEVICE_NAME_TABLET = '33003bf42aed6501'
+    APP =  r'C:\Users\20115\Downloads\app-release.apk'
+    COMMAND_EXE = 'http://127.0.0.1:4723/wd/hub'
